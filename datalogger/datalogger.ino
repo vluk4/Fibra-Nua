4d55ce0b4c6fee0d472e4e66c80010cb5506d6cd
Victor Luca Chagas <vluca002@gmail.com>
2018-11-19 11:13:50 -0300

    Update datalogger.ino

--- datalogger/datalogger.ino
+++ datalogger/datalogger.ino
@@ -3,55 +3,113 @@
    Data: 30 ago 18 Hora: 09:00
    REFERÊNCIA: https://www.youtube.com/watch?v=zRlONKtpC8M&feature=youtu.be
    */
-   
-#include <SD.h>  //Biblioteca cartão SD
-#define CS_Pin 3  // Pin digital 3  comunicação do Módulo SD 
-#define temp_pin A0 //Sensor de temperatura no pino analógico 0
 
-// ===== Configurações Iniciais 
+// --- Bibliotecas Auxiliares ---
+#include <SD.h>                      //Biblioteca cartão SD
 
-void setup() {
-Serial.begin (9600);
-Serial.println ("Iniciando o SD card"); // Frase inicial do módulo
-pinMode(3, OUTPUT);  // Configura a sáida 
 
-// Verifica se o cartão está pronto 
-if Serial.begin (3)
-{ 
-  Serial.println ("SD Card off");
-  return;   
-  
-} 
-  Serial.println (" SD Card conection successful");
-  
- // Cria e escreve o Arquivo  
- File.logfile = SD.open ("Fibra_nua.txt", FILE_WRITE); 
+// --- Mapeamento de Hardware ---
+#define CS_pin      10               //Comunicação SPI, CS_pin no digital 10
+#define temp_pin    A0               //Sensor de temperatura no pino analógico 0
+#define voltage_pin A1               //Sensor de tensão no pino analógico 1
 
-if (logfile)
+// --- Protótipo das Funções Auxiliares ---
+float AVG_TEMP();                  //Função para calcular a média de temperatura
+float AVG_VOLTAGE();               //Função para calcular a média de tensão
+
+
+// --- Variáveis Globais ---
+const int voltage = 5;              //Alimentação do Arduino
+const int celsius_unity = 100;      //Variação em Celsius
+const int ad_resolution = 1023;     //Resolução do conversor AD utilizada (10 bits)
+
+float temp_celsius;               //Variável de armazenamento
+float ts_volts;
+
+// --- Configurações Iniciais ---
+void setup()
 {
-  String data = "T_Celsius";
-  logfile.println (data);   
-   logfile.close (); 
-   Serial.println (data); 
-  } // end logfile 
-  else 
-  { 
-    Serial.println ("Erro ao abrir o arquivo");
-    }
-} // end setup 
+  Serial.begin(9600);                     //Inicia comunicação serial com 9600 de baud rate
+  Serial.println("Iniciando SD Card");    //Imprime frase
+  
+  pinMode(CS_pin, OUTPUT);                //Configura o pino CS como saída
+  
+  // -- Verifica se cartão SD está pronto --
+  if(!SD.begin(CS_pin))
+  {
+    Serial.println("Falha ao abrir cartao SD");
+    return;
+  }
+    
+  Serial.println("Cartao SD iniciado com sucesso!");
+  
+  
+  //Cria e escreve arquivo CSV
+  File logFile = SD.open("LOG5.csv", FILE_WRITE);
+  if(logFile)
+  {
+    logFile.println(","); // 
+    String header = "T_Celsius, Voltage";
+    logFile.println(header);
+    logFile.close();
+    Serial.println(header);
+  } //end if logFile
+  else
+  {
+    Serial.println("Erro ao abrir arquivo");
+  }
+  
+} //end void setup
 
-// ===
 
-void loop() {
-
-// Abrir um arquivo de cada vez 
-File logfile = SD.open (" Fibra_nua. txt", FILE_WRITE); 
-if (logfile )
+// --- Loop Infinito ---
+void loop()
 {
-  logfile.println(); 
-  logfile.close (); 
-  Serial.println (); 
-} // end logfile
+  //Cálculo de resolução AD e conversão Celsius ---------------- |
+  temp_celsius = AVG_TEMP() * voltage * celsius_unity / ad_resolution;
+  ts_volts = AVG_VOLTAGE() * voltage / ad_resolution;
+  
+  //Cria string de dados para armazenar no cartão SD
+  //Utilizando arquivo do tipo Comma Separete Value
+  String dataString = String(temp_celsius) + ", " + String(ts_volts);
+  
+  //Abre o arquivo para escrita
+  //Apenas um arquivo pode ser aberto de cada vez
+  File logFile = SD.open("LOG5.csv", FILE_WRITE);
+  if(logFile)
+  {
+    logFile.println(dataString);
+    logFile.close();
+    Serial.println(dataString);
+  } //end if logFile
+  else
+  {
+    Serial.println("Erro ao abrir arquivo para escrita final");
+  }
+  
+  delay(3000);
   
 
+} //end void loop
+
+float AVG_TEMP() //retorna a média entre 60 leituras de temperatura
+{
+  float temp_store = 0;
+  
+  for(int i=0; i<60; i++)
+  {
+    temp_store += analogRead(temp_pin);
+  }
+  
+  return(temp_store/60);
+}
+float AVG_VOLTAGE() //retorna a média entre 60 leituras de tensão
+{
+  float ts_store = 0;
+  
+  for(int i=0; i<60; i++)
+  {
+    ts_store += analogRead(voltage_pin);
+  }
+  return(ts_store/60);
 }
